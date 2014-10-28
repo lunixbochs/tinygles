b378c82fb277df80f0268a93fdf91e9859d22ebc
Ryan Hileman <lunixbochs@gmail.com>
2014-10-28 01:22:31 -0700

    revert to glshim pixel.c

--- src/gles/pixel.c
+++ src/gles/pixel.c
@@ -4,6 +4,7 @@
 
 #include "pixel.h"
 #include "gl_helpers.h"
+#include "gl_str.h"
 
 static const colorlayout_t *get_color_map(GLenum format) {
     #define map(fmt, ...)                               \
@@ -21,7 +22,7 @@ static const colorlayout_t *get_color_map(GLenum format) {
         map(GL_RGB, 0, 1, 2, -1);
         map(GL_RGBA, 0, 1, 2, 3);
         default:
-            printf("get_color_map(): Unknown pixel format 0x%x\n", format);
+            printf("get_color_map(): Unsupported pixel format %s\n", gl_str(format));
             break;
     }
     static colorlayout_t null = {0};
@@ -68,20 +69,20 @@ bool remap_pixel(const GLvoid *src, GLvoid *dst,
         type_case(GL_DOUBLE, GLdouble, read_each(,))
         type_case(GL_FLOAT, GLfloat, read_each(,))
         case GL_UNSIGNED_INT_8_8_8_8_REV:
-        type_case(GL_UNSIGNED_BYTE, GLubyte, read_each(, / 255.0))
-        type_case(GL_UNSIGNED_INT_8_8_8_8, GLubyte, read_each(3 - , / 255.0))
+        type_case(GL_UNSIGNED_BYTE, GLubyte, read_each(, / 255.0f))
+        type_case(GL_UNSIGNED_INT_8_8_8_8, GLubyte, read_each(3 - , / 255.0f))
         type_case(GL_UNSIGNED_SHORT_1_5_5_5_REV, GLushort,
             s = (GLushort[]){
                 v & 31,
-                (v & 0x03e0 >> 5) / 31.0,
-                (v & 0x7c00 >> 10) / 31.0,
-                (v & 0x8000 >> 15) / 31.0,
+                (v & 0x03e0 >> 5),
+                (v & 0x7c00 >> 10),
+                (v & 0x8000 >> 15)* 31,
             };
-            read_each(,);
+            read_each(, / 31.0f);
         )
         default:
             // TODO: add glSetError?
-            printf("remap_pixel(): Unsupported source data type: %i\n", src_type);
+            printf("remap_pixel(): Unsupported source data type: %s\n", gl_str(src_type));
             return false;
             break;
     }
@@ -111,8 +112,19 @@ bool remap_pixel(const GLvoid *src, GLvoid *dst,
                  ((GLuint)(color[2] * 31) & 0x1f << 10)  |
                  ((GLuint)(color[3] * 1)  & 0x01 << 15);
         )
+       type_case(GL_UNSIGNED_SHORT_4_4_4_4, GLushort,
+            GLfloat color[4];
+            color[dst_color->red] = pixel.r;
+            color[dst_color->green] = pixel.g;
+            color[dst_color->blue] = pixel.b;
+            color[dst_color->alpha] = pixel.a;
+            *d = ((GLushort)(color[0] * 15) & 0x0f << 12) |
+                 ((GLushort)(color[1] * 15) & 0x0f << 8) |
+                 ((GLushort)(color[2] * 15) & 0x0f << 4) |
+                 ((GLushort)(color[3] * 15) & 0x0f);
+        )
         default:
-            printf("remap_pixel(): Unsupported target data type: 0x%x\n", dst_type);
+            printf("remap_pixel(): Unsupported target data type: %s\n", gl_str(dst_type));
             return false;
             break;
     }
@@ -129,97 +141,6 @@ bool pixel_convert(const GLvoid *src, GLvoid **dst,
                    GLuint width, GLuint height,
                    GLenum src_format, GLenum src_type,
                    GLenum dst_format, GLenum dst_type) {
-    if (src_format == GL_RGBA && dst_format == GL_BGRA && src_type == GL_UNSIGNED_BYTE && dst_type == GL_UNSIGNED_SHORT_5_6_5) {
-        int size = (width * height);
-        GLushort *out = *dst = malloc(size * 2);
-        const GLubyte *pixels = (const GLubyte *)src;
-        for (int i = 0; i < size; i++) {
-            char r, g, b;
-            r = pixels[0] * 0x1f / 0xff;
-            g = pixels[1] * 0x3f / 0xff;
-            b = pixels[2] * 0x1f / 0xff;
-
-            *out++ = ((r & 0x1f) << 11) | ((g & 0x3f) << 5) | (b & 0x1f);
-            pixels += 4;
-        }
-        return true;
-    }
-    if (src_format == GL_RGBA && dst_format == GL_BGRA && src_type == dst_type && src_type == GL_UNSIGNED_BYTE) {
-        int size = (width * height);
-        GLubyte *out = *dst = malloc(size * 4);
-#ifdef __ARM_NEON__
-        asm volatile (
-            "mov r0, #0\n"
-            ".loop:\n"
-                "pld [%0]\n"
-                "vld4.8 {d0, d1, d2, d3}, [%0]\n"
-                "vswp d0, d2\n"
-                "vst4.8 {d0, d1, d2, d3}, [%1]\n"
-
-                "add %0, %0, #32\n"
-                "add %1, %1, #32\n"
-
-                "add r0, r0, #8\n"
-                "cmp r0, %2\n"
-                "blt .loop\n"
-            :
-            : "r"(src), "r"(out), "r"(width * height)
-            : "r0", "d0", "d1", "d2", "d3"
-        );
-        /*
-        asm volatile (
-            "pld [%0]\n"
-
-            "mov r0, #0xFF000000\n"
-            "vdup.32 q3, r0\n"
-            "mov r0, #0x0000FF00\n"
-            "vdup.32 q4, r0\n"
-            "lsl r0, r0, #8\n"
-            "orr r0, r0, #0xFF\n"
-            "vdup.32 q5, r0\n"
-
-            "mov r0, %2\n"
-            ".loop:\n"
-                "vld1.32 {d0, d1}, [%0]\n"
-                "vld1.32 {d2, d3}, [%0]\n"
-                "vld1.32 {d4, d5}, [%0]\n"
-
-                "vand.u32 q0, q3\n"
-                "vand.u32 q1, q4\n"
-                "vand.u32 q2, q5\n"
-
-                "vshr.u32 q0, q0, #16\n"
-                "vshl.u32 q1, q1, #16\n"
-
-                "vorr q0, q0, q1\n"
-                "vorr q0, q0, q2\n"
-
-                "vst1.32 {d0, d1}, [%1]\n"
-
-                "add %0, %0, #4\n"
-                "add %1, %1, #4\n"
-
-                "sub r0, r0, #1\n"
-                "cmp r0, #0\n"
-                "bgt .loop\n"
-            :
-            : "r"(src), "r"(out), "r"(width * height - 2)
-            : "r0", "d0", "d1", "d2", "d3"
-        );
-        */
-#else
-        const GLubyte *pixels = (const GLubyte *)src;
-        for (int i = 0; i < size; i++) {
-            out[0] = pixels[2];
-            out[1] = pixels[1];
-            out[2] = pixels[0];
-            out[3] = pixels[3];
-            out += 4;
-            pixels += 4;
-        }
-#endif
-        return true;
-    }
     const colorlayout_t *src_color, *dst_color;
     GLuint pixels = width * height;
     GLuint dst_size = pixels * gl_pixel_sizeof(dst_format, dst_type);
